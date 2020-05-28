0439f1a6fba2e06af7869bdb33f5f107c1c98f1f
Tiago Uruba <turuba@bionexo.com>
2020-05-28 12:55:38 -0300

    Added for create dynamic-block, multi-providers, workspace

---
+++ dynamic-block/sem-dynamic-block/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform"
+}
+
+resource "aws_security_group" "sem-dynamic-block" {
+  name        = "ficaemcasa-sg"
+  description = "SG de teste, sem dynamic-block"
+
+  ingress {
+    description = "Inbound para SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["127.0.0.1/32"]
+  }
+
+  ingress {
+    description = "Inbound para Jboss"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["127.0.0.1/32"]
+  }
+
+  ingress {
+    description = "Inbound para HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["127.0.0.1/32"]
+  }
+
+  ingress {
+    description = "Inbound para HTTPs"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["127.0.0.1/32"]
+  }
+
+  ingress {
+    description = "Inbound para postgres"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["127.0.0.1/32"]
+  }
+
+  tags = {
+    Name = "ficaemcasa"
+  }
+}
